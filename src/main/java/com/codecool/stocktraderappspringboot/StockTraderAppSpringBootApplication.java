579ecbe07aa9c447a4527f43963c3388f2bbac55
Kattkovacs <kattkovacs@gmail.com>
2020-11-01 18:26:54 +0100

    Initial commit

---
+++ src/main/java/com/codecool/stocktraderappspringboot/StockTraderAppSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.stocktraderappspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockTraderAppSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockTraderAppSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codecool/stocktraderappspringboot/StockTraderAppSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.stocktraderappspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockTraderAppSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
